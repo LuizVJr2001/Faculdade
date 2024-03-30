3f3cc5e6a3f5f277ea6691246523be8418ae9dd1
LuizVJr2001 <luizdevargasjr@gmail.com>
2024-03-30 19:58:18 -0300

    inicial

---
+++ algoritmo_e_programacao/area_triangulo.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 13:11:21 2024
+
+@author: luizdevjr
+"""
+
+b = float(input("Entre com o valor da base: "))
+h = float(input("Entre com o valor da altura: "))
+
+a= b*h/2
+print(f'Área={a}')
---
+++ algoritmo_e_programacao/jnotebook/exercicio.2.1.7.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 24 21:10:40 2024
+
+@author: luizdevjr
+"""
+
+import math as m
+print(f'Número pi = {m.pi}')
+print(f'Número de Euler e = {m.e}')
