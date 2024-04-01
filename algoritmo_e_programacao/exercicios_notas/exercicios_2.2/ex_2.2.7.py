227023d09a4ea2218c440d1bf30a5d3f0a77008d
LuizVJr2001 <luizdevargasjr@gmail.com>
2024-03-31 21:37:03 -0300

    ex_2.2.7

---
+++ algoritmo_e_programacao/exercicios_notas/exercicios_2.2/ex_2.2.7.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 31 21:31:39 2024
+
+@author: luizd
+"""
+
+a = float(input("Entre com o Valor de a: "))
+if (a != 0):
+    b = float(input("Entre com o Valor de b: "))
+    
+    x = -b/a
+    print(f'O valor de x é: {x}')
+else:
+    print("O valor de a não pode ser 0!")
