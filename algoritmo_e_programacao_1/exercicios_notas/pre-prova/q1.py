b5c6eae6d59a478ce6be16f61a27b992ac689703
LuizVJr2001 <luizdevargasjr@gmail.com>
2024-07-14 22:29:31 -0300

    q1_pre-prova_algoritos

--- algoritmo_e_programacao_1/exercicios_notas/pre-prova/q1.py
+++ algoritmo_e_programacao_1/exercicios_notas/pre-prova/q1.py
@@ -5,7 +5,10 @@ Created on Wed May  1 15:24:01 2024
 @author: luizd
 """
 
-u = (1, -1, 0)
-v = (1, 0, 2)
-w = (1, -1, 1)
+u = [1, -1, 0]
+v = [1, 0, 2]
+w = [1, -1, 1]
 
+prodmisto = (u[0]*v[1]*w[2] + u[1]*v[2]*w[0] + u[2]*v[0]*w[1] - u[2]*v[1]*w[0] - u[1]*v[0]*w[2] - u[0]*v[2]*w[1])
+
+print(f"produto misto = {prodmisto}")
